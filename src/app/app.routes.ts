60f725f33f6f75198bb408162718ac37471fe1ba
SovereIYN <158160896+SovereIYN@users.noreply.github.com>
2024-09-26 14:32:33 +0800

    try new app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
@@ -8,11 +9,22 @@ export const routes: Routes = [
         component: HomeComponent,
         title: "Home",
     },
-
     {
         path: "portfolio",
         component: PortfolioComponent,
         title: "Portfolio"
+    },
+    {
+        path: '**', // Redirects any unknown path to Home
+        redirectTo: '',
+        pathMatch: 'full'
     }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })], // Use Hash Location Strategy
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
 
